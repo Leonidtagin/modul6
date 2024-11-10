08822f63770150d4781ad95b690adeedab9aff47
Игорёха <leonidtagin401@gmail.com>
2024-11-11 04:47:12 +1100

    ...

--- урок.Наследование.py
+++ урок.Наследование.py
@@ -1,8 +1,10 @@
 class Animal:
-    def __init__(self, alive, fed, name):
-        self.alive = True
-        self.fed = False
-        self.name = 'Волк из ну погоди'
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+
 
 class Mammal(Animal):
     def eat(self, food):
@@ -15,34 +17,38 @@ class Mammal(Animal):
 
 
 class Predator(Animal):
-    def eat(self, food, name):
+    def eat(self, food):
         if isinstance(food, Plant) and food.edible:
             print('Съел')
             self.fed = True
         else:
             print('Не стал есть')
             self.alive = False
-            self.name = name
 
 class Plant:
-    def __init__(self, edible, name):
+    def __init__(self, name):
+        super().__init__()
         self.edible = False
-        self.name = 'Цветик семицветик'
+
 
 class Flower(Plant):
-    def __init__(self):
-        self.type = type
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+        pass
 
 class Fruit(Plant):
     def __init__(self, name):
-        self.type = type
-a1 = Predator('Волк из ну погоди')
-a2 = Mammal('Рекс')
+        super().__init__(name)
+        self.edible = True
+
+a1 = Predator('Волк с уолл стрит')
+a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводное яблоко')
+p2 = Fruit('Заводной апельсин')
 
 print(a1.name)
-print(p1.name)
+print(p1)
 
 print(a1.alive)
 print(a2.fed)
